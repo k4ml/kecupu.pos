8442282b0295c211a031d8d516dcfca730e1cf11
k4ml <kamal.mustafa@gmail.com>
2010-09-01 04:53:13 +0800

    setup url for admin

--- kecupu/pos/urls.py
+++ kecupu/pos/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 # URL patterns for kecupu.pos
 
 urlpatterns = patterns('kecupu.pos.views',
   # Add url patterns here
+  # TODO: This should include from kecupu.pos.app_urls
   (r'^$', 'index'),
+  (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
