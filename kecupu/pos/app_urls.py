805635d01a1511eebf35f0e462d812d7682c32fb
k4ml <kamal.mustafa@gmail.com>
2010-09-18 23:19:15 +0800

    Issue #3 - autocomplete search customer

--- kecupu/pos/app_urls.py
+++ kecupu/pos/app_urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('kecupu.pos.views',
     (r'^$', 'index'),
     (r'^order/new$', 'new_order'),
+    (r'^customer/autocomplete$', 'customer_autocomplete'),
 )
--- kecupu/pos/views.py
+++ kecupu/pos/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
 from kecupu.pos.utils import render_response, login_required
 
 @login_required
@@ -22,3 +24,16 @@ def new_order(request):
         'kecupu.pos/new_order.html',
         {'items': items}
     )
+
+@login_required
+def customer_autocomplete(request):
+    if 'term' in request.GET:
+        keyword = request.GET['term']
+        from kecupu.pos.models import Customer
+        import simplejson
+
+        customers = Customer.objects.filter(name__icontains=keyword)
+        output = []
+        for customer in customers:
+            output.append({"id": customer.id, "value": customer.id, "label": customer.name})
+        return HttpResponse(simplejson.dumps(output), mimetype='application/json')
