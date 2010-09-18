0140eced60ac920c3ab33fa9e7ea1b9ec1240674
k4ml <kamal.mustafa@gmail.com>
2010-09-18 18:14:08 +0800

    add customer and store models

--- kecupu/pos/admin.py
+++ kecupu/pos/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 # Add your admin site registrations here, eg.
-# from kecupu.pos.models import Author
-# admin.site.register(Author)
+from kecupu.pos.models import Customer, Store
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ['name']
+admin.site.register(Customer, CustomerAdmin)
+
+class StoreAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Store, StoreAdmin)
--- kecupu/pos/models.py
+++ kecupu/pos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
